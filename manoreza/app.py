a04a1edb221988141ff59fd81edca9e442b95179
masoodkamyab <masoodkamyab@users.noreply.github.com>
2023-07-24 17:00:42 +0000

    ✅ Ready to clone and code.

--- manoreza/app.py
+++ manoreza/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+manoreza API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="manoreza",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://manoreza.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "masoodkamyab",
+        "url": "http://manoreza.com/contact/",
+        "email": "masoodkamyab@manoreza.com",
     },
     license_info={
         "name": "The Unlicense",
--- manoreza/config.py
+++ manoreza/config.py
@@ -5,11 +5,11 @@ from dynaconf import Dynaconf
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 settings = Dynaconf(
-    envvar_prefix="project_name",
+    envvar_prefix="manoreza",
     preload=[os.path.join(HERE, "default.toml")],
     settings_files=["settings.toml", ".secrets.toml"],
     environments=["development", "production", "testing"],
-    env_switcher="project_name_env",
+    env_switcher="manoreza_env",
     load_dotenv=False,
 )
 
@@ -18,7 +18,7 @@ settings = Dynaconf(
 # How to use this application settings
 
 ```
-from project_name.config import settings
+from manoreza.config import settings
 ```
 
 ## Acessing variables
@@ -45,15 +45,15 @@ KEY=value
 
 ### As environment variables
 ```
-export PROJECT_NAME_KEY=value
-export PROJECT_NAME_KEY="@int 42"
-export PROJECT_NAME_KEY="@jinja {{ this.db.uri }}"
-export PROJECT_NAME_DB__uri="@jinja {{ this.db.uri | replace('db', 'data') }}"
+export manoreza_KEY=value
+export manoreza_KEY="@int 42"
+export manoreza_KEY="@jinja {{ this.db.uri }}"
+export manoreza_DB__uri="@jinja {{ this.db.uri | replace('db', 'data') }}"
 ```
 
 ### Switching environments
 ```
-PROJECT_NAME_ENV=production project_name run
+manoreza_ENV=production manoreza run
 ```
 
 Read more on https://dynaconf.com
